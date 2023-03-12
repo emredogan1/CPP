1837edb13a732b868f3e6f130746dc9208457b10
emredogan1 <116231573+emredogan1@users.noreply.github.com>
2023-03-12 22:52:08 +0300

    Update ArrayLinearSearch92.cpp

--- ArrayLinearSearch92.cpp
+++ ArrayLinearSearch92.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main()
 {
- 	int A[10],n=10;
+ 	int A[10];
 	int key;
 	cout<<"Enter Numbers";
 	    
-	for(int i=0;i<n;i++)
+	for(int i=0;i<10;i++)
 	{
 		cin>>A[i];
 	}
